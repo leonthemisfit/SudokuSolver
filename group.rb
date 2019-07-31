595d62de4dbe9a0c5b4031105732d4d245c3feec
leonthemisfit <leonthemisfit@gmail.com>
2019-07-31 10:45:47 -0400

    Added Group class
    The Group class serves as a container for cells. The intended use being
    to collect Squares into rows, columns, and boxes and provide some
    helpful methods for checking and manipulating entire groups of cells.
    
    On branch master
    Changes to be committed:
    	new file:   group.rb

---
+++ group.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Sudoku
+  class Group
+    attr_reader :squares
+
+    def initialize
+      @squares = []
+    end
+
+    def add_square(square)
+      @squares << square
+    end
+
+    def <<(square)
+      add_square(square)
+    end
+
+    def each(&block)
+      @squares.each { |sq| block.call(sq) }
+    end
+
+    def values
+      vals = @squares.map { |square| square.value }
+      vals.reject { |val| val.nil? || val.zero? }
+    end
+
+    def needed
+      (1..9).to_a - values
+    end
+
+    def valid?
+      values.length == values.uniq.length
+    end
+
+    def complete?
+      needed.length.zero?
+    end
+  end
+end
+
